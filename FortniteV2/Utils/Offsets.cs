480d6324bf32657f158a4f9489193ed457a1f183
stan <77831846+stanuwu@users.noreply.github.com>
2024-04-22 19:58:04 +0200

    update offset dumper

--- FortniteV2/Utils/Offsets.cs
+++ FortniteV2/Utils/Offsets.cs
@@ -49,7 +49,7 @@ namespace FortniteV2.Utils
         public static int m_iszPlayerName;
 
         // bones
-        public static readonly Dictionary<string, int> BONES = new Dictionary<string, int>
+        public static readonly Dictionary<string, int> BONES = new()
         {
             { "head", 6 },
             { "neck_0", 5 },
@@ -73,51 +73,52 @@ namespace FortniteV2.Utils
         // reading
         public static void ReadOffsets(string runDir)
         {
-            var path = runDir + "/offset_dumper/generated/";
+            var path = runDir + "/offset_dumper/output/";
             var offsetsText = File.ReadAllText(path + "offsets.json");
             var clientDllText = File.ReadAllText(path + "client.dll.json");
-            var offsets = (JObject)JObject.Parse(offsetsText)["client_dll"]?["data"];
-            dwEntityList = ParseOffset(offsets, "dwEntityList");
-            dwLocalPlayerPawn = ParseOffset(offsets, "dwLocalPlayerPawn");
-            dwLocalPlayerController = ParseOffset(offsets, "dwLocalPlayerController");
-            dwViewAngles = ParseOffset(offsets, "dwViewAngles");
-            dwViewMatrix = ParseOffset(offsets, "dwViewMatrix");
+            var offsets = (JObject)JObject.Parse(offsetsText)["client.dll"];
+            dwEntityList = ParseOffset(offsets, "dwEntityList", true);
+            dwLocalPlayerPawn = ParseOffset(offsets, "dwLocalPlayerPawn", true);
+            dwLocalPlayerController = ParseOffset(offsets, "dwLocalPlayerController", true);
+            dwViewAngles = ParseOffset(offsets, "dwViewAngles", true);
+            dwViewMatrix = ParseOffset(offsets, "dwViewMatrix", true);
 
-            var clientDll = JObject.Parse(clientDllText);
-            var basePlayerPawn = (JObject)clientDll["C_BasePlayerPawn"]?["data"];
+            var clientDll = (JObject)JObject.Parse(clientDllText)?["client.dll"]?["classes"];
+            var basePlayerPawn = (JObject)clientDll["C_BasePlayerPawn"];
             m_vOldOrigin = ParseOffset(basePlayerPawn, "m_vOldOrigin");
 
-            var baseModelEntity = (JObject)clientDll["C_BaseModelEntity"]?["data"];
+            var baseModelEntity = (JObject)clientDll["C_BaseModelEntity"];
             m_vecViewOffset = ParseOffset(baseModelEntity, "m_vecViewOffset");
 
-            var csPlayerPawn = (JObject)clientDll["C_CSPlayerPawn"]?["data"];
+            var csPlayerPawn = (JObject)clientDll["C_CSPlayerPawn"];
             m_aimPunchAngle = ParseOffset(csPlayerPawn, "m_aimPunchAngle");
 
-            var baseEntity = (JObject)clientDll["C_BaseEntity"]?["data"];
+            var baseEntity = (JObject)clientDll["C_BaseEntity"];
             m_lifeState = ParseOffset(baseEntity, "m_lifeState");
             m_iTeamNum = ParseOffset(baseEntity, "m_iTeamNum");
             m_iHealth = ParseOffset(baseEntity, "m_iHealth");
             m_pGameSceneNode = ParseOffset(baseEntity, "m_pGameSceneNode");
 
-            var gameSceneNode = (JObject)clientDll["CGameSceneNode"]?["data"];
+            var gameSceneNode = (JObject)clientDll["CGameSceneNode"];
             m_bDormant = ParseOffset(gameSceneNode, "m_bDormant");
 
-            var csPlayerPawnBase = (JObject)clientDll["C_CSPlayerPawnBase"]?["data"];
+            var csPlayerPawnBase = (JObject)clientDll["C_CSPlayerPawnBase"];
             m_ArmorValue = ParseOffset(csPlayerPawnBase, "m_ArmorValue");
             m_iShotsFired = ParseOffset(csPlayerPawnBase, "m_iShotsFired");
             m_iIDEntIndex = ParseOffset(csPlayerPawnBase, "m_iIDEntIndex");
 
-            var skeletonInstance = (JObject)clientDll["CSkeletonInstance"]?["data"];
+            var skeletonInstance = (JObject)clientDll["CSkeletonInstance"];
             m_modelState = ParseOffset(skeletonInstance, "m_modelState");
 
-            var basePlayerController = (JObject)clientDll["CBasePlayerController"]?["data"];
+            var basePlayerController = (JObject)clientDll["CBasePlayerController"];
             m_hPawn = ParseOffset(basePlayerController, "m_hPawn");
             m_iszPlayerName = ParseOffset(basePlayerController, "m_iszPlayerName");
         }
 
-        private static int ParseOffset(this JObject offsetsJson, string name)
+        private static int ParseOffset(this JObject offsetsJson, string name, bool offsets_root = false)
         {
-            return (int)offsetsJson[name]?["value"];
+            if (offsets_root) return (int)offsetsJson[name];
+            return (int)offsetsJson["fields"]?[name];
         }
     }
 }
